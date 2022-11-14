7c546801174f607914ed934b95364cf15c73cee5
MaxEgr <garagedetailingomsk@gmail.com>
2022-11-14 23:14:57 +0600

    PP_2_3_1

---
+++ src/main/java/web/dao/UserDAO.java
@@ -0,0 +1,48 @@
+package web.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import web.models.User;
+
+import java.util.List;
+@Component
+public class UserDAO {
+
+    private final SessionFactory sessionFactory;
+    @Autowired
+    public UserDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    @Transactional(readOnly = true)
+    public List <User> index(){
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("select p from user p", User.class)
+                .getResultList();
+    }
+    @Transactional(readOnly = true)
+    public User show(int id){
+        Session session = sessionFactory.getCurrentSession();
+            return session.get(User.class, id);
+    }
+    @Transactional
+    public void save(User user) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(user);
+    }
+    @Transactional
+    public void update(int id, User updatedUser){
+        Session session = sessionFactory.getCurrentSession();
+        User userToBeUpdated = session.get(User.class, id);
+        userToBeUpdated.setName(updatedUser.getName());
+        userToBeUpdated.setAge(updatedUser.getAge());
+        userToBeUpdated.setEmail(updatedUser.getEmail());
+    }
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(User.class, id));
+    }
+}
---
+++ src/main/java/web/models/User.java
@@ -0,0 +1,69 @@
+package web.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotEmpty
+    @Size(min=2, max=30)
+    @Column(name = "name")
+    private String name;
+
+    @Size(min=0)
+    @Column(name = "age")
+    private int age;
+
+    @NotEmpty
+    @Email
+    @Column(name = "email")
+    private String email;
+
+
+    public User(){}
+
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
